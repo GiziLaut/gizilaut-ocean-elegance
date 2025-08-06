1b3cfd0ddf53131af6289177e8b164002315bde1
Gizi Laut <gizilaut.id@gmail.com>
2025-08-06 08:26:32 +0700

    fix swiper products

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import './App.css';
 
 // ✅ Tambahkan redirect logic di sini
 const redirectedPath = sessionStorage.redirect;
