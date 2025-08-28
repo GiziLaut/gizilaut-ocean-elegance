cf0d4a5f6b3412e9ad123358abb680319a354be9
Gizi Laut <gizilaut.id@gmail.com>
2025-08-28 13:16:15 +0700

    build config + favicon tidy 2

--- vite.config.ts
+++ vite.config.ts
@@ -3,27 +3,18 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig({
-base: "/", // deploy di root domain
-server: {
-host: "0.0.0.0",
-port: 8080
-},
+base: "/",
+server: { host: "0.0.0.0", port: 8080 },
 plugins: [react()],
-resolve: {
-alias: {
-"@": path.resolve(__dirname, "./src")
-}
-},
+resolve: { alias: { "@": path.resolve(__dirname, "./src") } },
 build: {
 target: "es2017",
 modulePreload: false,
 cssTarget: "chrome61",
-outDir: "dist", // pastikan deploy folder ini
-assetsDir: "assets", // default, eksplisitkan saja
+outDir: "dist",
+assetsDir: "assets",
 sourcemap: false
 },
 esbuild: { target: "es2017" },
 optimizeDeps: { esbuildOptions: { target: "es2017" } }
-// Jika ada error “Unexpected token ?” dari dependency:
-// ssr: { noExternal: true }
 });
